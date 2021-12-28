858dc62b1066d765f2560aff0b96a57db81d4319
Nathan Schmidt <nschmdt@gmail.com>
2021-12-27 21:26:11 -0600

    Update header layout

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -7,26 +7,22 @@ import Nav from 'react-bootstrap/Nav';
 
 function Header() {
     return (
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <Stack direction="horizontal" gap="3">
-                            <PictureOfMe height="60" width="60" id="profile_pic" />
-                            <span>Nathan Schmidt</span>
-                        </Stack>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#features">Portfolio</Nav.Link>
-                            <Nav.Link href="#pricing">Contact</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <Stack direction="horizontal" gap="3">
+                        <PictureOfMe height="60" width="60" id="profile_pic" />
+                        <span>Nathan Schmidt</span>
+                    </Stack>
+                </Navbar.Brand>
+                <Nav className="justify-content-end">
+                    <Nav.Item><Nav.Link href="#home">Home</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="#features">Portfolio</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="#pricing">Contact</Nav.Link></Nav.Item>
+                </Nav>
+                    
+            </Container>
+        </Navbar>
     )
 }
 
