2e6d72096106087fc17903678e3e752621664559
Nathan Schmidt <nschmdt@gmail.com>
2021-12-28 11:15:24 -0600

    update colors

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -7,7 +7,7 @@ import Nav from 'react-bootstrap/Nav';
 
 function Header() {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="#8EB8E5" variant="light">
             <Container>
                 <Navbar.Brand href="#home">
                     <Stack direction="horizontal" gap="3">
@@ -16,11 +16,10 @@ function Header() {
                     </Stack>
                 </Navbar.Brand>
                 <Nav className="justify-content-end">
-                    <Nav.Item><Nav.Link href="#home">Home</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="#features">Portfolio</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="#pricing">Contact</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/portfolio">Portfolio</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
                 </Nav>
-                    
             </Container>
         </Navbar>
     )
