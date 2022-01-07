494bda98c83f84e2ac8b7729dc526335b1d4314a
Nathan Schmidt <nschmdt@gmail.com>
2022-01-07 13:04:14 -0600

    Initial Commit

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import PictureOfMe from './picture'
 
 function Home() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <h1 className='bold'>Hello, I'm Nathan Schmidt</h1>
+            <h4>Web Developer, OnBase Developer</h4>
+            <PictureOfMe height="300" width="300" id="mainPictureOfMe"></PictureOfMe>
+        </Container>
     )
 }
 
