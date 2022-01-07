bd899b465be9080de549fb17250e6c77784ecf3a
Nathan Schmidt <nschmdt@gmail.com>
2022-01-07 13:04:06 -0600

    Simplified header nav

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -7,21 +7,13 @@ import Nav from 'react-bootstrap/Nav';
 
 function Header() {
     return (
-        <Navbar bg="#8EB8E5" variant="light">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <Stack direction="horizontal" gap="3">
-                        <PictureOfMe height="60" width="60" id="profile_pic" />
-                        <span>Nathan Schmidt</span>
-                    </Stack>
-                </Navbar.Brand>
-                <Nav className="justify-content-end">
+        <>
+                <Nav className="justify-content-center">
                     <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/portfolio">Portfolio</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
                 </Nav>
-            </Container>
-        </Navbar>
+        </>
     )
 }
 
